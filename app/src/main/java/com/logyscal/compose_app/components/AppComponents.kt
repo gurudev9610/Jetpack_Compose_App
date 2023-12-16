fe1de2b45bb6d6b3606f07d5bcd4e00bdce74b7b
Sanjaya Prakash Pradhan <sanjayaprakashpradhan@logyscal.com>
2023-12-16 17:21:17 +0530

    new updates

--- app/src/main/java/com/logyscal/compose_app/components/AppComponents.kt
+++ app/src/main/java/com/logyscal/compose_app/components/AppComponents.kt
@@ -1,9 +1,15 @@
 package com.logyscal.compose_app.components
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
@@ -12,19 +18,24 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.logyscal.compose_app.ui.theme.TextColor
 import com.logyscal.compose_app.ui.theme.Primary
 import com.logyscal.compose_app.ui.theme.componentShapes
+import com.logyscal.compose_app.R
 
 //import androidx.compose.ui.res.colorResource
-//import com.logyscal.compose_app.R
-
 @Composable
 fun NormalTextComponent(value : String){
     Text(
@@ -63,14 +74,16 @@ fun HeadingTextComponent(value : String){
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyTextFieldComponent(labelValue:String) {
+fun EmailTextFieldComponent(labelValue: String, painterResource: Painter) {
 
     val textValue = remember {
         mutableStateOf(value = "")
     }
 
     OutlinedTextField(
-        modifier = Modifier.fillMaxWidth().clip(componentShapes.small),
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(componentShapes.small),
         label = { Text(text = labelValue) },
         colors = TextFieldDefaults.outlinedTextFieldColors(
             focusedBorderColor = Primary,
@@ -83,5 +96,67 @@ fun MyTextFieldComponent(labelValue:String) {
         onValueChange = {
             textValue.value = it
         },
+        leadingIcon = {
+            Icon(painter = painterResource, contentDescription = "")
+        }
+    )
+}
+
+@SuppressLint("ResourceType")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PasswordTextFieldComponent(labelValue: String, painterResource: Painter) {
+
+    val password = remember {
+        mutableStateOf(value = "")
+    }
+    val passwordVisible = remember {
+        mutableStateOf(value = false)
+    }
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(componentShapes.small),
+        label = { Text(text = labelValue) },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Primary,
+            focusedLabelColor = Primary,
+            cursorColor = Primary
+//          focusedBorderColor = colorResource(id = R.color.colorPrimary)
+        ),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        value = password.value,
+        onValueChange = {
+            password.value = it
+        },
+        leadingIcon = {
+            Icon(painter = painterResource, contentDescription = "")
+        },
+        trailingIcon = {
+            val iconImage = if(passwordVisible.value){
+                Icons.Filled.Visibility
+            }
+            else{
+                Icons.Filled.VisibilityOff
+            }
+
+            var description = if (passwordVisible.value){
+                stringResource(id = R.string.hide_pswd)
+            }
+            else{
+                stringResource(id = R.string.show_pswd)
+            }
+            
+            IconButton(onClick = { passwordVisible.value = !passwordVisible.value }) {
+                Icon(imageVector = iconImage, contentDescription = description)
+            }
+        },
+        visualTransformation = if (passwordVisible.value) {
+            VisualTransformation.None
+        }
+        else{
+            PasswordVisualTransformation()
+        }
     )
 }
--- app/src/main/java/com/logyscal/compose_app/screens/SignUpScreen.kt
+++ app/src/main/java/com/logyscal/compose_app/screens/SignUpScreen.kt
@@ -2,19 +2,23 @@ package com.logyscal.compose_app.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.logyscal.compose_app.R
+import com.logyscal.compose_app.components.EmailTextFieldComponent
 import com.logyscal.compose_app.components.HeadingTextComponent
-import com.logyscal.compose_app.components.MyTextFieldComponent
 import com.logyscal.compose_app.components.NormalTextComponent
+import com.logyscal.compose_app.components.PasswordTextFieldComponent
 
 @Composable
 fun SignUpScreen(){
@@ -26,11 +30,26 @@ fun SignUpScreen(){
             .background(Color.White)
             .padding(28.dp)
 
-    ) {
+        )
+    {
         Column(modifier = Modifier.fillMaxSize()) {
             NormalTextComponent(value = stringResource(id = R.string.hello))
             HeadingTextComponent(value = stringResource(id = R.string.signup))
-            MyTextFieldComponent(labelValue = stringResource(id = R.string.email))
+            Spacer(modifier = Modifier.height(20.dp))
+            EmailTextFieldComponent(
+                labelValue = stringResource(id = R.string.email),
+                painterResource(id = R.drawable.round_email_24)
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            PasswordTextFieldComponent(
+                labelValue = stringResource(id = R.string.create_pswd),
+                painterResource = painterResource(id = R.drawable.round_lock_24)
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            PasswordTextFieldComponent(
+                labelValue = stringResource(id = R.string.confirm_pswd),
+                painterResource = painterResource(id = R.drawable.round_lock_24)
+            )
         }
     }
 }
